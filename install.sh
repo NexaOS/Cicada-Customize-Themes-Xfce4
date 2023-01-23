ba24f0367b149816e6195120257febe2b78582d4
NexaOS <nexalinux@gmail.com>
2023-01-23 13:09:31 -0800

    1-23-2023

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+################################
+RED=$(tput setaf 1 && tput bold)
+GREEN=$(tput setaf 2 && tput bold)
+STAND=$(tput sgr0)
+BLUE=$(tput setaf 6 && tput bold)
+################################
